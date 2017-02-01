755c76f63e2db8df87b31ec343754cfcc4aad323
suddat <suddat.shende@gmail.com>
2017-02-01 23:53:21 +0530

    first file for pass

---
+++ first.js
@@ -0,0 +1,27 @@
+/* // first three videos
+function main(anotherFun, stuff){
+	stuff(anotherFun);
+}
+
+main("Hello Suddat",function(stuff){console.log(__dirname);});*/
+
+/* 
+function printStuff() {
+	console.log("After 3 sec!");
+}
+
+setTimeout(printStuff,3000);*/
+
+/*
+function printStuff() {
+	console.log("After 3 sec!");
+}
+
+setInterval(printStuff,3000);*/
+
+//Kunal*17
+
+//Suddat21
+
+
+//interaction no---131823560
